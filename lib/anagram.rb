8a83f035173caeb9a3fd5c557f72c4f5828b6a64
Jonathan Alexander <jonathandalexander@gmail.com>
2016-06-10 13:19:03 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)    #listen.match([....])
+    initial_word = @word.split("").sort  #listen => ["l", "i", "s", "t", "e", "n"] 
+
+    array.each_with_object([]) do |word_to_compare, matches|
+      matches << word_to_compare if initial_word == word_to_compare.split("").sort
+    end
+  end
+  # => return all matches in an array or empty array
+end
+
+
+#break word into array of chars .split("")
+#iterate over each word in array (for match method)
+# => break word into array of chars .split("")
+
